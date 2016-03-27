d6a37bc20ca23e788e9058b0433c6c7c1e9148fb
DanielJDufour <daniel.j.dufour@gmail.com>
2016-03-27 01:10:33 -0400

    Fixed dependency build

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
   packages = ['location_extractor'],
   package_dir = {'location_extractor': 'location_extractor'},
   package_data = {'location_extractor': ['prep/char_language.txt','tests/__init__.py','tests/test.py']},
-  version = '2.7',
+  version = '2.8',
   description = 'Extract locations from text',
   author = 'Daniel J. Dufour',
   author_email = 'daniel.j.dufour@gmail.com',
