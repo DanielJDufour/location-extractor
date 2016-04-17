8a3556c40fec39fadc3e7118217c8c9f0f021075
unknown <unknown>
2016-04-17 07:22:12 +0000

    Merging

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
   packages = ['location_extractor'],
   package_dir = {'location_extractor': 'location_extractor'},
   package_data = {'location_extractor': ['*']},
-  version = '3.2',
+  version = '3.4',
   description = 'Extract locations from text',
   author = 'Daniel J. Dufour',
   author_email = 'daniel.j.dufour@gmail.com',
